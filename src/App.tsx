0c57bcb0b79c956240acfaccd072eb258e237972
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-06 15:50:01 +0530

    connected app to redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
-const App = () => {
+import { Todo, fetchTodos } from './redux/actions';
+import { StoreState } from './redux/reducers/root.reducer';
+
+interface AppProps {
+  todos?: Todo[];
+  fetchTodos?: () => any;
+}
+
+const _App = (props: AppProps): JSX.Element => {
+  const { todos, fetchTodos } = props;
+
   return (
     <div className="App">
       <h1>Hi there!</h1>
@@ -9,4 +20,18 @@ const App = () => {
   );
 }
 
-export default App;
+
+interface MapStateType {
+  todos: Todo[]
+}
+
+const mapStateToProps = (state: StoreState): MapStateType => {
+  return ({
+    todos: state.todos,
+  });
+}
+
+export const App = connect(
+  mapStateToProps,
+  null
+)(_App);
